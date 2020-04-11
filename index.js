d425709a121d7bcd838b4c3da489b690fb3cd646
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-04-11 22:45:54 +0530

    Assignment-1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
@@ -11,6 +13,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname+ '/public'));
